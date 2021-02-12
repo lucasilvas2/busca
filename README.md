093e2dab5f47822ee75762c28a8e32a87472b774
Lucas <lucasoliver1068@gmail.com>
2021-02-12 00:29:17 -0300

    Adicionando funçao  para definir o alcance a partir do tamanho do vetor

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Iniciar o terminal na pasta do arquivo fork.cpp
     Compilar o arquivo utilizando o comando:
         $ g++ -std=c++11 busca.cpp -o busca
 
-    Executar o aquivo fork com o comando:
+    Executar o aquivo fork com o comando e passar por linha de comando o valor para o tamanho do vetor que deseja:
         $ ./busca
 
 ```
--- busca.cpp
+++ busca.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+int alcance_min(int tam_vetor);
+int alcance_max(int tam_vetor);
 //Função para gerar valores aleatorios
 //Utiliza o min e max para gerar os numeros de forma crescente
 int gerando(int min, int max){
@@ -35,7 +37,6 @@ int busca_linear(int k, int colecao[], int tamanho){
     int posicao = 0;
     do
     {
-        //cout << "Pos = " << colecao[posicao] << endl;
         if(k == colecao[posicao]){
             return posicao;
         }
@@ -46,10 +47,11 @@ int busca_linear(int k, int colecao[], int tamanho){
 
 int main(int argc, char const *argv[])
 {
+    int entrada = atoi(argv[1]);
     //O min/max utiliza para definir um range para a geracao dos valores e garantir que seja de forma crescente
     int min = 1 , max = 5;
     //Alterar para o valor do tamanho do vetor que deseja criar
-    int tam_lista = 3000;
+    int tam_lista = entrada;
 
     int lista[tam_lista];
     lista[0] = 0; //Garantido que a lista inicie com 0
@@ -68,12 +70,14 @@ int main(int argc, char const *argv[])
     min = 0;
     max = 1;
     int pos = 0;
-    for(int i = 0; i < 20; i++){
+    for(int i = 0; i < 30; i++){
         
         x = gerando(min, max);
         //Valores do min/max pode ser alterado manualmente para que na busca alcance o último elemento do vetor
-        min += 5;
-        max += 10;
+        //min += 58;
+        //max += 95;
+        min += alcance_min(tam_lista);
+        max += alcance_max(tam_lista);
         //cout << "Buscando o número: " << x << endl;
         auto inicio_linear = chrono::steady_clock::now();
         pos = busca_linear(x, lista, tam_lista);
@@ -84,18 +88,20 @@ int main(int argc, char const *argv[])
         time_total_linear += time;
         //cout << "Time = " << time << endl;
     }
-    
+    //cout << "Buscando o número: " << x << endl;
     cout << "Time Total Linear = " << time_total_linear << endl;
-    cout << "Media Linear= " << (time_total_linear / 20) << endl << endl << endl;
-    
+    cout << "Media Linear= " << (time_total_linear / 30) << endl << endl;
+    pos = 0;
     min = 0;
     max = 1;
-    for(int i = 0; i < 20; i++){
+    for(int i = 0; i < 30; i++){
         
         x = gerando(min, max);
         //Valores do min/max pode ser alterado manualmente para que na busca alcance o último elemento do vetor
-        min += 5;
-        max += 1;
+        //min += 58;
+        //max += 95;
+        min += alcance_min(tam_lista);
+        max += alcance_max(tam_lista);
         //cout << "Buscando o número: " << x << endl;
         auto inicio_binaria = chrono::steady_clock::now();
         pos = busca_binaria(x, lista, tam_lista);
@@ -106,10 +112,127 @@ int main(int argc, char const *argv[])
         time_total_binaria += time;
         //cout << "Time = " << time << endl;
     }
-    
+    //cout << "Buscando o número: " << x << endl;
     cout << "Time Total Binaria= " << time_total_binaria << endl;
-    cout << "Media Binaria = " << (time_total_binaria / 20) << endl;
+    cout << "Media Binaria = " << (time_total_binaria / 30) << endl;
     
     
     return 0;
+}
+
+int alcance_min(int tam_vetor){
+    int min;
+    if(tam_vetor <= 10){
+        min = 3;
+    }  
+    else if(tam_vetor > 10 && tam_vetor<= 20){
+        min = 5;
+    }
+    else if(tam_vetor > 20 && tam_vetor<= 50){
+        min = 9;
+    }
+    else if(tam_vetor > 50 && tam_vetor<= 100){
+        min = 18;
+    }
+    else if(tam_vetor > 100 && tam_vetor<= 150){
+        min = 27;
+    }
+    else if(tam_vetor > 150 && tam_vetor<= 200){
+        min = 35;
+    }
+    else if(tam_vetor > 200 && tam_vetor<= 250){
+        min = 42;
+    }
+    else if(tam_vetor > 250 && tam_vetor<= 300){
+        min = 59;
+    }
+    else if(tam_vetor > 300 && tam_vetor<= 350){
+        min = 63;
+    }
+    else if(tam_vetor > 350 && tam_vetor<= 400){
+        min = 69;
+    }
+    else if(tam_vetor > 400 && tam_vetor<= 600){
+        min = 76;
+    }
+    else if(tam_vetor > 600 && tam_vetor<= 800){
+        min = 81;
+    }
+    else if(tam_vetor > 800 && tam_vetor<= 1000){
+        min = 90;
+    }
+    else if(tam_vetor > 1000 && tam_vetor<= 1500){
+        min = 103;
+    }
+    else if(tam_vetor > 1500 && tam_vetor<= 2000){
+        min = 110;
+    }
+    else if(tam_vetor > 2000 && tam_vetor<= 2500){
+        min = 119;
+    }
+    else if(tam_vetor > 2500 && tam_vetor<= 3000){
+        min = 153;
+    }
+    else{
+        min = 186;
+    }
+    return min;
+}
+int alcance_max(int tam_vetor){
+    int max;
+    if(tam_vetor <= 10){
+        max = 5;
+    }  
+    else if(tam_vetor > 10 && tam_vetor<= 20){
+        max = 9;
+    }
+    else if(tam_vetor > 20 && tam_vetor<= 50){
+        max = 17;
+    }
+    else if(tam_vetor > 50 && tam_vetor<= 100){
+        max = 31;
+    }
+    else if(tam_vetor > 100 && tam_vetor<= 150){
+        max = 69;
+    }
+    else if(tam_vetor > 150 && tam_vetor<= 200){
+        max = 79;
+    }
+    else if(tam_vetor > 200 && tam_vetor<= 250){
+        max = 83;
+    }
+    else if(tam_vetor > 250 && tam_vetor<= 300){
+        max = 96;
+    }
+    else if(tam_vetor > 300 && tam_vetor<= 350){
+        max = 105;
+    }
+    else if(tam_vetor > 350 && tam_vetor<= 400){
+        max = 118;
+    }
+    else if(tam_vetor > 400 && tam_vetor<= 600){
+        max = 127;
+    }
+    else if(tam_vetor > 600 && tam_vetor<= 800){
+        max = 138;
+    }
+    else if(tam_vetor > 800 && tam_vetor<= 1000){
+        max = 145;
+    }
+    else if(tam_vetor > 1000 && tam_vetor<= 1500){
+        max = 156;
+    }
+    else if(tam_vetor > 1500 && tam_vetor<= 2000){
+        max = 172;
+    }
+    else if(tam_vetor > 2000 && tam_vetor<= 2500){
+        max = 189;
+    }
+    else if(tam_vetor > 2500 && tam_vetor<= 3000){
+        max = 200;
+    }
+    else{
+        max = 250;
+    }
+    return max;
 }
